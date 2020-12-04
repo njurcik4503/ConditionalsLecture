db3adbc5a76fd39f442617ae6291ec227ee0965a
natalie <njurcik4503@stu.d214.org>
2020-12-03 20:03:43 -0600

    Initial Commit

---
+++ ConditionalsLecture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConditionalsLecture
+//
+//  Created by Natalie Jurcik on 12/3/20.
+//  Copyright © 2020 Natalie Jurcik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
